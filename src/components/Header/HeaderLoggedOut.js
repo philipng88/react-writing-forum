5b66e56d2bdf64fbc43479d34b4367fe819a6650
philipng88 <philiplyng@gmail.com>
2020-05-22 15:19:54 -0600

    provide validation feedback for empty fields in login form

--- src/components/Header/HeaderLoggedOut.js
+++ src/components/Header/HeaderLoggedOut.js
@@ -4,31 +4,62 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
 import axios from "axios";
+import classNames from "classnames";
 import DispatchContext from "../../context/DispatchContext";
 
 const HeaderLoggedOut = () => {
   const [username, setUsername] = useState();
   const [password, setPassword] = useState();
+  const [usernameIsEmpty, setUsernameIsEmpty] = useState();
+  const [passwordIsEmpty, setPasswordIsEmpty] = useState();
   const appDispatch = useContext(DispatchContext);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      const response = await axios.post("/login", {
-        username,
-        password,
-      });
-      if (response.data) {
-        appDispatch({ type: "login", data: response.data });
-        appDispatch({ type: "flashMessage", value: "Successfully logged in" });
-      } else {
+      if (username && password) {
+        setUsernameIsEmpty(false);
+        setPasswordIsEmpty(false);
+        const response = await axios.post("/login", {
+          username,
+          password,
+        });
+        if (response.data) {
+          appDispatch({ type: "login", data: response.data });
+          appDispatch({
+            type: "flashMessage",
+            value: "Successfully logged in",
+          });
+        } else {
+          appDispatch({
+            type: "flashMessage",
+            value: "Incorrect username and/or password",
+          });
+        }
+      } else if (!username && password) {
+        setUsernameIsEmpty(true);
+        setPasswordIsEmpty(false);
         appDispatch({
           type: "flashMessage",
-          value: "Incorrect username and/or password",
+          value: "Please enter your username",
+        });
+      } else if (username && !password) {
+        setUsernameIsEmpty(false);
+        setPasswordIsEmpty(true);
+        appDispatch({
+          type: "flashMessage",
+          value: "Please enter your password",
+        });
+      } else {
+        setUsernameIsEmpty(true);
+        setPasswordIsEmpty(true);
+        appDispatch({
+          type: "flashMessage",
+          value: "Please enter your username and password",
         });
       }
     } catch (error) {
-      console.log(error.response.data);
+      console.log(error);
     }
   };
 
@@ -40,7 +71,9 @@ const HeaderLoggedOut = () => {
             onChange={(event) => setUsername(event.target.value)}
             size="sm"
             name="username"
-            className="input-dark"
+            className={classNames("input-dark", {
+              "is-invalid pr-2": usernameIsEmpty,
+            })}
             type="text"
             placeholder="Username"
             autoComplete="off"
@@ -51,7 +84,9 @@ const HeaderLoggedOut = () => {
             onChange={(event) => setPassword(event.target.value)}
             size="sm"
             name="password"
-            className="input-dark"
+            className={classNames("input-dark", {
+              "is-invalid pr-2": passwordIsEmpty,
+            })}
             type="password"
             placeholder="Password"
           />
